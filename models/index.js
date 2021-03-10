4342f4dd8ff4bb6e3c167770ce34a20af0423397
gwarz <gmwarzecha@tutanota.com>
2021-03-10 15:05:43 -0700

    added get, post, put, and delete routes for user posts

--- models/index.js
+++ models/index.js
@@ -1,5 +1,15 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// craete associations 
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
+
+// defines the relationship of the Post model to the User; post can belong to 1 user, not many
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
 // exports an object with the User model as a property
 module.exports = { User, Post };
