041e873f852d226a1340e71c06532349768d241e
gwarz <gmwarzecha@tutanota.com>
2021-03-12 14:00:55 -0700

    heroku troubleshoot

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
